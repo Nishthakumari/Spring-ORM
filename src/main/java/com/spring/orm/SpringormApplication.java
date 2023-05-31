48cafe2f5eee3e677093f85b50dc3dd735085a72
Nishthakumari <knishtha0@gmail.com>
2023-05-31 10:31:52 +0530

    Complete Configuration of Spring ORM step by step

--- src/main/java/com/spring/orm/SpringormApplication.java
+++ src/main/java/com/spring/orm/SpringormApplication.java
@@ -1,10 +1,20 @@
 package com.spring.orm;
 
 
+import com.spring.orm.dao.StudentDao;
+import com.spring.orm.entities.Student;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class SpringormApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+		StudentDao studentDao = context.getBean("studentDao", StudentDao.class);
+		Student student= new Student(1223, "Nishtha", "lucknow");
+
+		int r = studentDao.insert(student);
+		System.out.println(r+ " done");
 	}
 
 }
--- src/main/java/com/spring/orm/dao/StudentDao.java
+++ src/main/java/com/spring/orm/dao/StudentDao.java
@@ -1,12 +1,22 @@
 package com.spring.orm.dao;
 
 import com.spring.orm.entities.Student;
+import jakarta.transaction.Transactional;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 
 public class StudentDao {
     private HibernateTemplate hibernateTemplate;
 
+    public StudentDao(HibernateTemplate hibernateTemplate) {
+        this.hibernateTemplate = hibernateTemplate;
+    }
+
+    public void setHibernateTemplate(HibernateTemplate hibernateTemplate) {
+        this.hibernateTemplate = hibernateTemplate;
+    }
+
     //save student
+    @Transactional
     public int insert(Student student)
     {
         Integer i = (Integer) this.hibernateTemplate.save(student);
