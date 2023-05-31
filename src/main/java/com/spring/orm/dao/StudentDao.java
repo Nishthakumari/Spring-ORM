76bd8d34e28e1988b4b8eadd26cb281827af4fc7
Nishthakumari <knishtha0@gmail.com>
2023-05-31 11:27:26 +0530

    Complete CRUD Operation using Spring ORM | Spring ORM Tutorial | Spring with Hibernate

--- src/main/java/com/spring/orm/dao/StudentDao.java
+++ src/main/java/com/spring/orm/dao/StudentDao.java
@@ -4,6 +4,8 @@ import com.spring.orm.entities.Student;
 import jakarta.transaction.Transactional;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 
+import java.util.List;
+
 public class StudentDao {
     private HibernateTemplate hibernateTemplate;
 
@@ -21,5 +23,28 @@ public class StudentDao {
     {
         Integer i = (Integer) this.hibernateTemplate.save(student);
         return i;
+    }
+
+    public Student getStudent(int studentId)
+    {
+        Student student = this.hibernateTemplate.get(Student.class, studentId);
+        return student;
+    }
+
+    public  List<Student> getAllstudents()
+    {
+        List<Student> students = this.hibernateTemplate.loadAll(Student.class);
+        return students;
+    }
+
+    @Transactional
+    public void deleteStudent(int studentId){
+        Student student  = this.hibernateTemplate.get(Student.class, studentId);
+        this.hibernateTemplate.delete(student);
+    }
+
+    @Transactional
+    public void updateStudent(Student student){
+        this.hibernateTemplate.update(student);
     }
 }
